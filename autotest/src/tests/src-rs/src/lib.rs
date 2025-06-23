39df993fac6f447882f493e1c38cdc00c9826ae1
Stephane N <stephane.neveu@ssi.gouv.fr>
2025-06-23 14:01:10 +0200

    Autotest: Use kconfig crate in libtest-rs

--- autotest/src/tests/src-rs/src/lib.rs
+++ autotest/src/tests/src-rs/src/lib.rs
@@ -17,17 +17,34 @@ mod test_shm;
 mod test_signal;
 mod test_sleep;
 mod test_yield;
+mod test_gpio;
+use kconfig;
 
 #[unsafe(no_mangle)]
 pub fn autotest() {
+    kconfig_import::import_dotconfig_from_script();
+
+    #[cfg(CONFIG_TEST_IPC)]
     test_ipc::test_ipc();
+    #[cfg(CONFIG_TEST_IRQ)]
     test_irq::test_irq();
+    #[cfg(CONFIG_TEST_DEVICES)]
     test_map::test_map();
+    #[cfg(CONFIG_TEST_RANDOM)]
     test_random::test_random();
+    #[cfg(CONFIG_TEST_SIGNALS)]
     test_signal::test_signal();
+    #[cfg(CONFIG_TEST_SHM)]
     test_shm::test_shm();
+    #[cfg(CONFIG_TEST_SLEEP)]
     test_sleep::test_sleep();
+    #[cfg(CONFIG_TEST_YIELD)]
     test_yield::test_yield();
+    #[cfg(CONFIG_TEST_DMA)]
     test_dma::test_dma();
+    #[cfg(CONFIG_TEST_CYCLES)]
     test_cycles::test_cycles();
+    #[cfg(CONFIG_TEST_GPIO)]
+    test_gpio::test_gpio();
 }
+
