2248f21b3166d74619f3f470dcde3a9b7be8acc7
Stephane N <stephane.neveu@ssi.gouv.fr>
2025-06-23 14:51:44 +0200

    Autotest: GPIO, fix call to led0

--- autotest/src/tests/src-rs/src/test_gpio.rs
+++ autotest/src/tests/src-rs/src/test_gpio.rs
@@ -12,6 +12,7 @@ use core::prelude::v1::Ok;
 use sentry_uapi::syscall::{get_device_handle, gpio_configure, gpio_set, gpio_toggle, sleep};
 use crate::test_suite_start;
 use crate::test_suite_end;
+use crate::devices_utils::get_device_by_name;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn test_gpio() -> bool {
@@ -20,9 +21,9 @@ pub extern "C" fn test_gpio() -> bool {
 
     ok &= test_gpio_toggle();
     ok &= test_gpio_off();
-    sleep(SleepDuration { tag: SLEEP_DURATION_ARBITRARY_MS, arbitrary_ms: 1000 }, SleepMode::Deep);
+    sleep(SleepDuration::ArbitraryMs(1000), SleepMode::Deep);
     ok &= test_gpio_on();
-    sleep(SleepDuration { tag: SLEEP_DURATION_ARBITRARY_MS, arbitrary_ms: 1000 }, SleepMode::Deep);
+    sleep(SleepDuration::ArbitraryMs(1000), SleepMode::Deep);
     ok &= test_gpio_off();
     ok &= test_gpio_invalid_io();
     ok &= test_gpio_invalid_devh();
@@ -34,7 +35,8 @@ pub extern "C" fn test_gpio() -> bool {
 fn test_gpio_on() -> bool {
     test_start!();
     let mut dev: DeviceHandle = 0;
-    let ok = check_eq!(get_device_handle(devices[DEV_ID_LED0].id), Status::Ok)
+    let device = get_device_by_name("led0").expect("LED0 device not found");
+    let ok = check_eq!(get_device_handle(device.id), Status::Ok)
         & (unsafe { copy_from_kernel(&mut (&mut dev as *mut _ as *mut u8)) } == Ok(Status::Ok));
     log_line!("USER_AUTOTEST_INFO", "handle is {:#x}", dev);
     let ok = ok
@@ -47,7 +49,8 @@ fn test_gpio_on() -> bool {
 fn test_gpio_off() -> bool {
     test_start!();
     let mut dev: DeviceHandle = 0;
-    let ok = check_eq!(get_device_handle(devices[DEV_ID_LED0].id), Status::Ok)
+    let device = get_device_by_name("led0").expect("LED0 device not found");
+    let ok = check_eq!(get_device_handle(device.id), Status::Ok)
         & (unsafe { copy_from_kernel(&mut (&mut dev as *mut _ as *mut u8)) } == Ok(Status::Ok));
     log_line!("USER_AUTOTEST_INFO", "handle is {:#x}", dev);
     let ok = ok
@@ -60,8 +63,9 @@ fn test_gpio_off() -> bool {
 fn test_gpio_toggle() -> bool {
     test_start!();
     let mut dev: DeviceHandle = 0;
-    let duration = SleepDuration { tag: SLEEP_DURATION_ARBITRARY_MS, arbitrary_ms: 250 };
-    let mut ok = check_eq!(get_device_handle(devices[DEV_ID_LED0].id), Status::Ok)
+    let duration = SleepDuration::ArbitraryMs(250);
+    let device = get_device_by_name("led0").expect("LED0 device not found");
+    let mut ok = check_eq!(get_device_handle(device.id), Status::Ok)
         & (unsafe { copy_from_kernel(&mut (&mut dev as *mut _ as *mut u8)) } == Ok(Status::Ok))
         & check_eq!(gpio_configure(dev, 0), Status::Ok);
         for _ in 0..10 {
@@ -75,7 +79,9 @@ fn test_gpio_toggle() -> bool {
     fn test_gpio_invalid_io() -> bool {
         test_start!();
         let mut dev: DeviceHandle = 0;
-        let ok = check_eq!(get_device_handle(devices[DEV_ID_LED0].id), Status::Ok)
+        let device = get_device_by_name("led0").expect("LED0 device not found");
+
+        let ok = check_eq!(get_device_handle(device.id), Status::Ok)
             & (unsafe { copy_from_kernel(&mut (&mut dev as *mut _ as *mut u8)) } == Ok(Status::Ok))
             & check_eq!(gpio_configure(dev, 4), Status::Invalid)
             & check_eq!(gpio_configure(dev, 8), Status::Invalid)
