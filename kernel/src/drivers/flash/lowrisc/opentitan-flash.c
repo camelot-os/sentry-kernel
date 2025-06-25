319e50db826d9debb9ad1517e595cc2c85dc50cb
lb-anssi <luc.bonnafoux@ssi.gouv.fr>
2025-06-25 15:48:31 +0200

    Added lowrisc drivers
    * Creation of dedicated repository for st drivers and modification of meson to select correct drivers for the platform
    * Addition of (simple) drivers for lowrisc opentitan

---
+++ kernel/src/drivers/flash/lowrisc/opentitan-flash.c
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 ANSSI
+// SPDX-License-Identifier: Apache-2.0
+
+#include <bsp/drivers/flash/flash.h>
+
+#include <sentry/ktypes.h>
+
+kstatus_t flash_probe(void) {
+    // TODO
+
+    return K_STATUS_OKAY;
+}
