b31c34cf80701aaf1375db23f203697f3233f04f
Philippe Thierry <phil@reseau-libre.net>
2025-07-29 14:05:44 +0200

    manager,security: fixing security init in autotest mode
    task mgr is not yet started, ro capa field is unused now

--- kernel/src/managers/security/security.c
+++ kernel/src/managers/security/security.c
@@ -42,17 +42,24 @@ kstatus_t mgr_security_init(void)
     __platform_enforce_alignment();
 #endif
 #if CONFIG_BUILD_TARGET_AUTOTEST
-    taskh_t autotest;
-    const task_meta_t *meta;
-    if (mgr_task_get_handle(0xbabe, &autotest) != K_STATUS_OKAY) {
-        panic(PANIC_CONFIGURATION_MISMATCH);
-    }
-    if (mgr_task_get_metadata(autotest, &meta) != K_STATUS_OKAY) {
-        /* current must be a valid task */
-        panic(PANIC_CONFIGURATION_MISMATCH);
-    }
-    autotest_cap_set = meta->capabilities;
+    /* set bootup capas for autotest here */
+    autotest_cap_set = 0;
+#ifdef CONFIG_TEST_DEVICES
+    autotest_cap_set |= CAP_DEV_BUSES;
 #endif
+#ifdef CONFIG_TEST_GPIO
+    autotest_cap_set |= CAP_DEV_IO;
+#endif
+#ifdef CONFIG_TEST_RANDOM
+    autotest_cap_set |= CAP_CRY_KRNG;
+#endif
+#ifdef CONFIG_TEST_DMA
+    autotest_cap_set |= CAP_DEV_DMA;
+#endif
+#ifdef CONFIG_TEST_IRQ
+    autotest_cap_set |= CAP_DEV_TIMER;
+#endif
+#endif /*CONFIG_BUILD_TARGET_AUTOTEST*/
     return status;
 }
 
