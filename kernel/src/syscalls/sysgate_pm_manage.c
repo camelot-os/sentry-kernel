57aab0c4ef06c9071e5e3d7584afacc676a52296
Philippe Thierry <phil@reseau-libre.net>
2025-08-01 09:23:48 +0200

    gate,fix: fixing all DMA & PM capability checks

--- kernel/src/syscalls/sysgate_pm_manage.c
+++ kernel/src/syscalls/sysgate_pm_manage.c
@@ -13,6 +13,10 @@ stack_frame_t *gate_pm_manage(stack_frame_t *frame, uint32_t pm_command)
     taskh_t current = sched_get_current();
     stack_frame_t *next_frame = frame;
 
+    if (unlikely(mgr_security_has_capa(current, CAP_SYS_POWER) != SECURE_TRUE)) {
+        mgr_task_set_sysreturn(current, STATUS_DENIED);
+        goto end;
+    }
     if (unlikely(pm_command > CPU_SLEEP_ALLOW_SLEEP)) {
         mgr_task_set_sysreturn(current, STATUS_INVALID);
         goto end;
